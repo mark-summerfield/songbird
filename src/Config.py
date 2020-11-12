c1c2204f346366eccb3eebd2f102ff120e276cc9
mark-summerfield <mark@qtrac.eu>
2020-11-12 08:52:13 +0000

    refactored Config - can't put it in a module due to circular import problem

--- src/Config.py
+++ src/Config.py
@@ -5,6 +5,9 @@ import collections
 import pathlib
 
 import apsw
+from ConfigConst import (
+    _CLEAR_RECENT_FILES, _CREATE, _GET, _GET_VERSION, _INCREMENT,
+    _INSERT_RECENT, _PREPARE, _SET, _UPDATE_VERSION, _VERSION)
 from Const import (
     APPNAME, LAST_FILE, MAIN_WINDOW_GEOMETRY, MAIN_WINDOW_STATE, OPENED,
     RECENT_FILE, RECENT_FILES_MAX, SHOW_AS_TABS, SHOW_CONTENTS,
@@ -214,9 +217,9 @@ class _Singleton_Config:
                 options.show_as_tabs = first(
                     cursor, _GET, dict(key=SHOW_AS_TABS), Class=bool)
                 for n in range(1, RECENT_FILES_MAX + 1):
-                    if name := first(
-                            cursor, _GET, dict(key=f'{RECENT_FILE}/{n}'),
-                            Class=str):
+                    if name := first(cursor,
+                                     _GET, dict(key=f'{RECENT_FILE}/{n}'),
+                                     Class=str):
                         name = pathlib.Path(name)
                         if name.exists():
                             options.recent_files.append(str(name.resolve()))
@@ -252,62 +255,3 @@ class _Singleton_Config:
             # 7 TODO
             # CREATE TABLE IF NOT EXISTS FILES (...
             # CREATE TABLE IF NOT EXISTS WINDOWS (...
-
-
-_VERSION = 6
-
-
-_PREPARE = f'''
-PRAGMA encoding = 'UTF-8';
-PRAGMA foreign_keys = TRUE;
-PRAGMA synchronous = NORMAL;
-PRAGMA temp_store = MEMORY;
-'''
-
-_CREATE = f'''
-PRAGMA user_version = {_VERSION};
-
-DROP TABLE IF EXISTS config;
-
-CREATE TABLE config (
-    key TEXT PRIMARY KEY NOT NULL,
-    value BLOB
-) WITHOUT ROWID;
-
-INSERT INTO config (key, value) VALUES ('{OPENED}', 1);
-INSERT INTO config (key, value) VALUES ('{MAIN_WINDOW_STATE}', NULL);
-INSERT INTO config (key, value) VALUES ('{MAIN_WINDOW_GEOMETRY}', NULL);
-INSERT INTO config (key, value) VALUES ('{LAST_FILE}', NULL);
-INSERT INTO config (key, value) VALUES ('{SHOW_CONTENTS}', TRUE);
-INSERT INTO config (key, value) VALUES ('{SHOW_PRAGMAS}', FALSE);
-INSERT INTO config (key, value) VALUES ('{SHOW_AS_TABS}', FALSE);
-
--- TODO
-/*
-CREATE TABLE files (
-    fid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    filename TEXT,
-    updated INTEGER DEFAULT STRFTIME('%s', 'NOW'),
-    -- etc.
-);
-
-CREATE TABLE windows (
-    -- etc.
-);
-*/
-'''
-
-_INSERT_RECENT = 'INSERT INTO config (key, value) VALUES (:key, NULL);'
-
-_INCREMENT = 'UPDATE config SET value = value + 1 WHERE key = :key;'
-
-_GET = 'SELECT value FROM config WHERE key = :key;'
-
-_SET = 'UPDATE config SET value = :value WHERE key = :key;'
-
-_CLEAR_RECENT_FILES = f'''
-UPDATE config SET value = NULL WHERE key LIKE '{RECENT_FILE}%';'''
-
-_GET_VERSION = 'PRAGMA user_version;'
-
-_UPDATE_VERSION = f'PRAGMA user_version = {_VERSION};'
---
+++ src/ConfigConst.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# Copyright © 2020 Mark Summerfield. All rights reserved.
+
+from Const import (
+    LAST_FILE, MAIN_WINDOW_GEOMETRY, MAIN_WINDOW_STATE, OPENED, RECENT_FILE,
+    SHOW_AS_TABS, SHOW_CONTENTS, SHOW_PRAGMAS)
+
+_VERSION = 6
+
+_PREPARE = f'''
+PRAGMA encoding = 'UTF-8';
+PRAGMA foreign_keys = TRUE;
+PRAGMA synchronous = NORMAL;
+PRAGMA temp_store = MEMORY;
+'''
+
+_CREATE = f'''
+PRAGMA user_version = {_VERSION};
+
+DROP TABLE IF EXISTS config;
+
+CREATE TABLE config (
+    key TEXT PRIMARY KEY NOT NULL,
+    value BLOB
+) WITHOUT ROWID;
+
+INSERT INTO config (key, value) VALUES ('{OPENED}', 1);
+INSERT INTO config (key, value) VALUES ('{MAIN_WINDOW_STATE}', NULL);
+INSERT INTO config (key, value) VALUES ('{MAIN_WINDOW_GEOMETRY}', NULL);
+INSERT INTO config (key, value) VALUES ('{LAST_FILE}', NULL);
+INSERT INTO config (key, value) VALUES ('{SHOW_CONTENTS}', TRUE);
+INSERT INTO config (key, value) VALUES ('{SHOW_PRAGMAS}', FALSE);
+INSERT INTO config (key, value) VALUES ('{SHOW_AS_TABS}', FALSE);
+
+-- TODO
+/*
+CREATE TABLE files (
+    fid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    filename TEXT,
+    updated INTEGER DEFAULT STRFTIME('%s', 'NOW'),
+    -- etc.
+);
+
+CREATE TABLE windows (
+    -- etc.
+);
+*/
+'''
+
+_INSERT_RECENT = 'INSERT INTO config (key, value) VALUES (:key, NULL);'
+
+_INCREMENT = 'UPDATE config SET value = value + 1 WHERE key = :key;'
+
+_GET = 'SELECT value FROM config WHERE key = :key;'
+
+_SET = 'UPDATE config SET value = :value WHERE key = :key;'
+
+_CLEAR_RECENT_FILES = f'''
+UPDATE config SET value = NULL WHERE key LIKE '{RECENT_FILE}%';'''
+
+_GET_VERSION = 'PRAGMA user_version;'
+
+_UPDATE_VERSION = f'PRAGMA user_version = {_VERSION};'
