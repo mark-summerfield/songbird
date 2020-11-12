951b24469cbda4b1adb50208b0b38a61d9781588
mark-summerfield <mark@qtrac.eu>
2020-11-12 09:03:01 +0000

    unlint

--- st.sh
+++ st.sh
@@ -4,6 +4,8 @@ tokei -f -slines -c110 -tPython
 unrecognized.py -q
 python3 -m flake8 --config=../setup.cfg . \
     | grep -v undefined.name..qApp
-python3 -m vulture . | grep -v '60%.confidence'
+python3 -m vulture . \
+    | grep -v '60%.confidence' \
+    | grep -v MainWindow.__init__.py.*unused.import
 cd ..
 git st
