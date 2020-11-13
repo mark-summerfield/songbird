9edb3f2f91170d483ab8252134930f71450ab7b5
mark-summerfield <mark@qtrac.eu>
2020-11-13 10:42:34 +0000

    lint

--- src/Config/__init__.py
+++ src/Config/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 # Copyright © 2020 Mark Summerfield. All rights reserved.
 
-from .Config import (MainWindowOptions, ToggleOptions, filename, get, set,
+from .Config import (MainWindowOptions, filename, get, set,
         write_main_window_options, read_main_window_options)
